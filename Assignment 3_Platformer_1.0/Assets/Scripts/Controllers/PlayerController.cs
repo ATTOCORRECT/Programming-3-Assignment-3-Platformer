4d7614cc98aa9f8a40793dfa27194b86258e7826
ATTOCORRECT <97545662+ATTOCORRECT@users.noreply.github.com>
2023-11-13 19:57:55 -0500

    collide and slide stuff

--- Assignment 3_Platformer_1.0/Assets/Scripts/Controllers/PlayerController.cs
+++ Assignment 3_Platformer_1.0/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     public enum FacingDirection { Left, Right }
-    public float topSpeed;
     public FacingDirection direction;
-    bool up, left, down, right, jump, crouch, special;
-    Rigidbody2D rigidbody;
 
-    Vector2 velocity = Vector2.zero;
+    public float topSpeed;
+    
+    bool up, left, down, right, jump, crouch, special; //input
+
+    Rigidbody2D rigidbody;
+    CircleCollider2D collider;
+
+    float skinWidth = 0.01f;
+    int maxBounces = 5;
+    Bounds bounds;
+
+    Vector2 velocity = Vector2.down;
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<CircleCollider2D>();
     }
 
 
@@ -48,67 +58,59 @@ public class PlayerController : MonoBehaviour
 
     void PlayerMovement()
     {
-        Gravity();
+        Walk();
 
 
+        //Gravity();
 
-        GroundCollision();
+
+        //GroundCollision();
 
 
-        rigidbody.position += velocity * Time.fixedDeltaTime;
+        //Collisions
+
+        bounds = collider.bounds;
+        bounds.Expand(-2 * skinWidth);
+
+        velocity =  CollideAndSlide(velocity, rigidbody.position, 0, false);
+        velocity += CollideAndSlide(Vector2.down * Time.fixedDeltaTime * 0.1f, rigidbody.position, 0, true);
+        rigidbody.position += velocity ;
     }
 
-    void GroundCollision()
+    Vector2 CollideAndSlide(Vector2 velocity, Vector2 position, int depth, bool gravityPass)
     {
-        Vector2 step = velocity * Time.fixedDeltaTime;
-
-        Debug.DrawRay(rigidbody.position - Vector2.one / 2, Vector2.right);
-        Debug.DrawRay(rigidbody.position - Vector2.one / 2, Vector2.up);
-        Debug.DrawRay(rigidbody.position + Vector2.one / 2, Vector2.left);
-        Debug.DrawRay(rigidbody.position + Vector2.one / 2, Vector2.down);
-
-        RaycastHit2D groundCollision = Physics2D.CircleCast(rigidbody.position, 0.5f, step, step.magnitude, LayerMask.GetMask("Ground"));
-
-        Debug.DrawRay(rigidbody.position, step);
-        Debug.DrawRay(rigidbody.position - Vector2.one/2 + step, Vector2.right);
-
-        if (groundCollision)
+        if(depth >= maxBounces)
         {
-            Debug.DrawRay(groundCollision.point + Vector2.left / 4, Vector2.right / 2, Color.cyan);
-            Debug.DrawRay(groundCollision.point + Vector2.up / 4, Vector2.down / 2, Color.cyan);
-
-            Vector2 newStep = Vector2.ClampMagnitude(step, groundCollision.distance);
-
-            Vector2 leftoverStep = step - newStep;
-
-            //Vector2 slideStep = << HERE (perp, swap x & y )
-
-            velocity = newStep / Time.fixedDeltaTime;
+            return Vector2.zero;
         }
+
+        float distance = velocity.magnitude + skinWidth;
+
+        RaycastHit2D hit = Physics2D.CircleCast(position, bounds.extents.x, velocity.normalized, distance, LayerMask.GetMask("Ground"));
+        if (hit)
+        {
+            Vector2 snapToSurface = velocity.normalized * (hit.distance - skinWidth);
+            Vector2 leftover = velocity - snapToSurface;
+
+            if(snapToSurface.magnitude <= skinWidth)
+            {
+                snapToSurface = Vector2.zero;
+            }
+
+            float mag = leftover.magnitude;
+            leftover = Vector3.ProjectOnPlane(leftover, hit.normal).normalized;
+            leftover *= mag;
+
+            return snapToSurface + CollideAndSlide(leftover, position + snapToSurface, depth + 1, gravityPass);
+        }
+
+        return velocity;
     }
 
-    Vector2 Project(Vector2 a, Vector2 b)
+    void Walk()
     {
-        return (Vector2.Dot(b, a) / b.magnitude * b.magnitude) * b;
+       
     }
-
-    void Gravity()
-    {
-        float terminalVelocity = 20; // units per second
-        float gravity = 20; // units per second per second
-
-        if (velocity.y > -terminalVelocity)
-        {
-            velocity += Vector2.down * gravity * Time.fixedDeltaTime;
-        }
-        else if (velocity.y < -terminalVelocity)
-        {
-            Vector3 currentVel = velocity;
-            velocity = new Vector2 (currentVel.x, -terminalVelocity);
-        }
-        
-    }
-
     void UpdateDirection()
     {
         if (left)  { direction = FacingDirection.Left; }

ee59a245976ae2d3f43ad90e3314ef15fe9b9ef4
Auto <97545662+ATTOCORRECT@users.noreply.github.com>
2023-11-14 10:16:25 -0500

    more collide

--- Assignment 3_Platformer_1.0/Assets/Scripts/Controllers/PlayerController.cs
+++ Assignment 3_Platformer_1.0/Assets/Scripts/Controllers/PlayerController.cs
@@ -13,17 +13,21 @@ public class PlayerController : MonoBehaviour
     bool up, left, down, right, jump, crouch, special; //input
 
     Rigidbody2D rigidbody;
-    CircleCollider2D collider;
 
-    float skinWidth = 0.01f;
+    Vector2[] corners = { new Vector2(-4f / 16f, -8f / 16f),
+                          new Vector2( 4f / 16f, -8f / 16f),
+                          new Vector2( 4f / 16f,  4f / 16f),
+                          new Vector2(-4f / 16f,  4f / 16f)};
+
     int maxBounces = 5;
-    Bounds bounds;
+    float skinWidth = 0.01f;
 
-    Vector2 velocity = Vector2.down;
+    Vector2 velocity;
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        collider = GetComponent<CircleCollider2D>();
+
+        velocity = Vector2.left * Time.fixedDeltaTime;
     }
 
 
@@ -33,6 +37,13 @@ public class PlayerController : MonoBehaviour
     }
 
     public bool CanJump()
+    {
+        float distanceAboveGround = (10f / 16f);
+        bool grounded = Physics2D.Raycast(transform.position, Vector2.down, distanceAboveGround, LayerMask.GetMask("Ground"));
+        return grounded;
+    }
+
+    public bool IsGrounded()
     {
         float distanceAboveGround = (10f / 16f);
         bool grounded = Physics2D.Raycast(transform.position, Vector2.down, distanceAboveGround, LayerMask.GetMask("Ground"));
@@ -54,25 +65,23 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         PlayerMovement();
+
+        //collision box debug
+        Debug.DrawLine(rigidbody.position + corners[0], rigidbody.position + corners[1]);
+        Debug.DrawLine(rigidbody.position + corners[1], rigidbody.position + corners[2]);
+        Debug.DrawLine(rigidbody.position + corners[2], rigidbody.position + corners[3]);
+        Debug.DrawLine(rigidbody.position + corners[3], rigidbody.position + corners[0]);
     }
 
     void PlayerMovement()
     {
-        Walk();
+        
 
 
         //Gravity();
-
-
-        //GroundCollision();
-
-
+        velocity += WalkVelocity();
         //Collisions
-
-        bounds = collider.bounds;
-        bounds.Expand(-2 * skinWidth);
-
-        velocity =  CollideAndSlide(velocity, rigidbody.position, 0, false);
+        velocity = CollideAndSlide(velocity, rigidbody.position, 0, false);
         velocity += CollideAndSlide(Vector2.down * Time.fixedDeltaTime * 0.1f, rigidbody.position, 0, true);
         rigidbody.position += velocity ;
     }
@@ -86,30 +95,42 @@ public class PlayerController : MonoBehaviour
 
         float distance = velocity.magnitude + skinWidth;
 
-        RaycastHit2D hit = Physics2D.CircleCast(position, bounds.extents.x, velocity.normalized, distance, LayerMask.GetMask("Ground"));
-        if (hit)
+        for (int i = 0; i < 4; i++)
         {
-            Vector2 snapToSurface = velocity.normalized * (hit.distance - skinWidth);
-            Vector2 leftover = velocity - snapToSurface;
+            RaycastHit2D hit = Physics2D.Raycast(position + corners[i], velocity.normalized, distance, LayerMask.GetMask("Ground"));
+            //Debug.DrawRay(position + corners[i], velocity, Color.white);
 
-            if(snapToSurface.magnitude <= skinWidth)
+            if (hit)
             {
-                snapToSurface = Vector2.zero;
+                Vector2 snapToSurface = velocity.normalized * (hit.distance - skinWidth);
+                Vector2 leftover = velocity - snapToSurface;
+
+/*                if(snapToSurface.magnitude <= skinWidth)
+                {
+                    snapToSurface = Vector2.zero;
+                }*/
+
+                float mag = leftover.magnitude;
+                leftover = Vector3.ProjectOnPlane(leftover, hit.normal).normalized;
+                leftover *= mag;
+
+                return snapToSurface + CollideAndSlide(leftover, position + snapToSurface, depth + 1, gravityPass);
             }
-
-            float mag = leftover.magnitude;
-            leftover = Vector3.ProjectOnPlane(leftover, hit.normal).normalized;
-            leftover *= mag;
-
-            return snapToSurface + CollideAndSlide(leftover, position + snapToSurface, depth + 1, gravityPass);
         }
 
         return velocity;
     }
 
-    void Walk()
+    Vector2 WalkVelocity()
     {
-       
+        Vector2 walkVelocity = Vector2.zero;
+
+        Debug.Log(velocity.x / Time.fixedDeltaTime);
+
+        if (left  && velocity.x / Time.fixedDeltaTime > -1) { walkVelocity += Vector2.left  * 1; }
+        if (right && velocity.x / Time.fixedDeltaTime <  1) { walkVelocity += Vector2.right * 1; }
+
+        return walkVelocity * Time.fixedDeltaTime;
     }
     void UpdateDirection()
     {
